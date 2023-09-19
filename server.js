e153d6060818c52c3e207ade845326b46e9f2b5b
Nicolas <Nicolas.rocca10@gmail.com>
2023-09-19 11:43:55 -0300

    HBS

--- server.js
+++ server.js
@@ -1,32 +1,44 @@
 const express = require('express');
 const passport = require('passport');
 const app = express();
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+//const cookieParser = require('cookie-parser');
+//const session = require('express-session');
 const PassportLocal = require('passport-local').Strategy;
 const path = require("path"); //Es para poner dirname, es una libreria de node
 require('dotenv').config({path:'./.env'}); //Esto es para poder traerlo desde el env
 require('dotenv').config(); //Con esto lo puedo leer como texto plano
+const mensajes = require('./mensajes.json');
 
 const usuario = process.env.user; //Declaro que le voy a poner usuario a lo que traiga desde env
 const contraseña = process.env.password; //Declaro que le voy a poner contraseña a lo que traiga desde env
 
 
+// https://www.npmjs.com/package/hbs
+app.set('view engine','hbs'); // Setea el motor de plantillas hbs (Handlebars)
+
+
+
+
+
+
 //comando para ejectuarlo nodemon: npm run start
 //comando para ejecutarlo con node: node app.js (app es el nombre del archivo)
 
+app.use(express.static('public'))
+
 app.use(express.urlencoded({extended: true}));
 
-app.use(cookieParser('Un secreto'));
+/*app.use(cookieParser('Un secreto'));
 
 app.use(session({
     secret: "Un secreto",
     resave: true,
     saveUninitialized: true
-}))
+}))*/
+
 
 app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.session());
 
 passport.use(new PassportLocal(function(username,password,done){
     if(username === "nicolas" && password === "123456")
@@ -49,19 +61,16 @@ passport.deserializeUser(function(id,done){
     done(null,{id: 1, name: "Nico"});
 })
 
-app.set('view engine','ejs')
 
-app.get('/',(req,res)=>{
-    res.render("index");
-}) 
 
-app.get("/",  (req,res,next) =>{
+
+/*app.get("/",  (req,res,next) =>{
     if(req.isAuthenticated()) return next(); //Eso lo que hace es, si está bien que siga para adelante con next. Si no vuelva a la pagina principal (en mi caso va a ser error)
 
     res.redirect("/login");
 }   ,(req,res)=>{
     res.render("mensajes");
-})
+})*/
 
 app.get("/login",(req,res)=>{
    //Mostrar el formulario
@@ -84,11 +93,30 @@ app.get('/contacto',(req,res)=>{
     res.render("contacto");
 })
 
-app.get('/index',(req,res)=>{
+/*app.get('/index',(req,res)=>{
     res.render("index");
-})
+})*/
 
  app.get('/mensajes',(req,res)=>{
-    res.render("mensajes");
+    res.render("mensajes", mensajes);
 }) 
 
+
+app.get("/", (req, res) => {
+    const usuario = {
+        nombre: "Charly",
+        edad: 30
+    }
+
+    // let {nombre, edad} = usuario; // PPT de Javascript contemporaneo
+    // Es buena practica que las referencias a objetos y arrays se guarden en constantes para evitar cambiarlas por accidente
+
+    console.table(mensajes);
+    //res.render("index.hbs", usuario);
+    res.render("index.hbs", {
+        mensajes, // PPT de Javascript contemporaneo,
+        coloreado: true
+    });
+});
+
+console.log(mensajes[0].fecha);
